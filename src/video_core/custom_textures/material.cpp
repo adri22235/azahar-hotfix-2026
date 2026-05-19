b9715139c18ad0dd8d9464d17b78993b44d2d20f
Adrián García Casado <adriangarciacasado42@gmail.com>
2026-05-19 16:30:54 +0200

    video_core: add license headers to custom texture material files

--- src/video_core/custom_textures/material.cpp
+++ src/video_core/custom_textures/material.cpp
@@ -1,3 +1,7 @@
+// Copyright Citra Emulator Project / Azahar Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
 // Copyright 2023 Citra Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
